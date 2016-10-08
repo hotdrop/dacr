ef6203b85801409dfc12579beedca3f9c484483c
kenji <gunsecondstage0106@gmail.com>
2016-10-08 17:03:01 +0900

    gradleプロジェクトに対応した

--- src/main/kotlin/dacr/main/AppStart.kt
+++ src/main/kotlin/dacr/main/AppStart.kt
@@ -4,24 +4,21 @@ import dacr.indata.RecordAttributeJson
 import dacr.model.Sphere
 import dacr.outdata.CsvFile
 
-object AppStart {
+fun main(args: Array<String>) {
 
-    @JvmStatic fun main(args: Array<String>) {
-
-        if(args.size != 3) {
-            println("usage: dacr [<inputFilePath>] [<outputFilePath>] [<create record number>]")
-            return
-        }
-
-        run(args[0], args[1], args[2].toInt())
+    if(args.size != 3) {
+        println("usage: dacr [<inputFilePath>] [<outputFilePath>] [<create record number>]")
+        return
     }
 
-    fun run(inPath: String, outPath: String, createNum: Int) {
-        print("start create data")
-        val columnList = RecordAttributeJson(inPath).parse()
-        val dataSphere = Sphere(columnList)
-        val csvFile = CsvFile(dataSphere, outPath, createNum)
-        csvFile.output()
-        print("complete!!")
-    }
+    run(args[0], args[1], args[2].toInt())
+}
+
+private fun run(inPath: String, outPath: String, createNum: Int) {
+    println("start create data")
+    val columnList = RecordAttributeJson(inPath).parse()
+    val dataSphere = Sphere(columnList)
+    val csvFile = CsvFile(dataSphere, outPath, createNum)
+    csvFile.output()
+    println("complete!!")
 }
